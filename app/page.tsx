5794251aa38624f8a31858401b5cbb5cdb9fc8cb
technocloudsolutions <technocloudsolution@gmail.com>
2024-10-31 22:22:01 +0530

    Initial commit

--- app/page.tsx
+++ app/page.tsx
@@ -4,9 +4,13 @@ import EnhancedExpenseTracker from '@/components/enhanced-expense-tracker';
 export default function Home() {
   return (
     <main className="container mx-auto p-2 sm:p-4 max-w-7xl">
-      <div className="space-y-4">
-        <Auth />
-        <EnhancedExpenseTracker />
+      <div className="space-y-6 sm:space-y-8">
+        <div className="relative z-10">
+          <Auth />
+        </div>
+        <div className="relative z-0">
+          <EnhancedExpenseTracker />
+        </div>
       </div>
     </main>
   );
